272247621a27b1b3db7aaa4ba197d43daf1b0274
Sean Pettit <s.p@mac.com>
2019-11-24 23:21:48 +0000

    animation calculations

--- src/Pages/Track/index.js
+++ src/Pages/Track/index.js
@@ -19,6 +19,8 @@ let Track = (props) => {
     const startDate = moment(state.currentTrack.start_time)
     const [theDate, setTheDate] = useState(startDate)
     const [lastPoint, setLastPoint] = useState({lat: 0, lng: 0})
+    const [nextPoint, setnextPoint] = useState({lat: 0, lng: 0})
+    const [markerCoords, setMarkerCoords] = useState({lat: 0, lng: 0})
     useEffect(() => getCurrentTrackById(props.track, dispatch),[dispatch, props.track])
 
     useEffect(() => {
@@ -30,21 +32,33 @@ let Track = (props) => {
                 if(points[i].timestamp > theDate.valueOf()){
                     console.log(true)
                     setLastPoint(points[i-1])
+                    setnextPoint(points[i])
                     return
                 }
             }}
-            }, [state.currentTrack.minified_points, theDate])
+            }, [setnextPoint, state.currentTrack.minified_points, theDate])
         
     
 
-
+    useEffect(() => {
+        const duration = parseInt(nextPoint.timestamp) - parseInt(lastPoint.timestamp)
+        // console.log("duration", duration)
+        const part = parseInt(theDate.valueOf()) - parseInt(lastPoint.timestamp)
+        // console.log("part", part)
+        const fraction = part/duration
+        const markerlat = lastPoint.lat + ((nextPoint.lat-lastPoint.lat)*fraction)
+        const markerlng = lastPoint.lng + ((nextPoint.lng-lastPoint.lng)*fraction)
+        console.log(markerlat, markerlng)
+        setMarkerCoords({lat: markerlat, lng: markerlng})
+        
+    }, [lastPoint.lat, lastPoint.lng, lastPoint.timestamp, nextPoint.lat, nextPoint.lng, nextPoint.timestamp, theDate])
 
 
    
     return (
         <div>
             <h1>{state.currentTrack.name}</h1>
-            <TrackMap marker={{lat: lastPoint.lat, lng: lastPoint.lng}}/>
+            <TrackMap marker={markerCoords}/>
             {/* <TrackPointsList /> */}
             <div>{theDate.format("DD/MM/YYYY HH:mm:ss")}</div>
             <input type="number" onChange={(e) => setTheDate(startDate.add(e.target.value, "m"))} />
